fb514aa5c879b73b1e6fa99c92f3ef455d7032d3
Aditya Niranjan Mishra <aditya.mishra6752@gmail.com>
2021-10-01 15:26:55 +0530

    backend done

--- routes/task.js
+++ routes/task.js
@@ -2,11 +2,7 @@
 const router = require("express").Router();
 const Task = require("../models/Task");
 
-router.get("/alltasks", function(req, res) {
-    return res.send("done");
-});
-
-router.post("/", async (req, res) => {
+router.post("/createtask", async (req, res) => {
     // console.log(req.body);
     const newTask = new Task(req.body);
     try {
@@ -14,6 +10,27 @@ router.post("/", async (req, res) => {
         res.status(200).json(savedTask);
     } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+router.delete("/deletetask/:id", async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        await task.deleteOne();
+        return res.status(200).send("you deleted this task successfully");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.get("/AllTask", function (req, res) {
+    try {
+        Task.find({}, (err, task) => {
+            return res.status(200).send(task);
+        });
+    } catch (err) {
+        return res.status(500).json(err);
     }
 });
 
