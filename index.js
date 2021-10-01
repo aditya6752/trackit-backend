ae5ad551b15d67a110bc1c737271fa61f6a1e524
Nilesh Singh <69386843+nilesh-som80@users.noreply.github.com>
2021-10-01 13:58:46 +0530

    start fixed and dot env activated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
