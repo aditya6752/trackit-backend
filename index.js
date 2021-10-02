a0f949d835f71f967f4451d79ddf1a107de629c5
Aditya Niranjan Mishra <aditya.mishra6752@gmail.com>
2021-10-02 13:09:49 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const task = require('./routes/task');
-
+const cors = require('cors');
 
 dotenv.config()
 const port = process.env.PORT || 5000
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true}, ()=>{
     console.log("Connected to DATABASE")
 });
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/task", task);
